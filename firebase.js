ae535d475c43eea62f015b1b7fe0d1e6dd1c336f
beethrodriguesbr-svg <beethrodrigues.br@gmail.com>
2026-03-02 20:27:04 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebase.js
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB6V7WPNBJ4SPFpQ9Edw7CLNTHAIY0G86o",
-  authDomain: "sistema-clinica-5c5b5.firebaseapp.com",
-  projectId: "sistema-clinica-5c5b5",
-  storageBucket: "sistema-clinica-5c5b5.firebasestorage.app",
-  messagingSenderId: "830320583010",
-  appId: "1:830320583010:web:630f5009bd3aad85a54c3b",
-  measurementId: "G-FZMNED3F84"
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
