379cf3380c308e811c2273139f65c1c23e533a4c
beethrodriguesbr-svg <beethrodrigues.br@gmail.com>
2026-03-02 21:21:30 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -4,12 +4,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyB6V7WPNBJ4SPFpQ9Edw7CLNTHAIY0G86o",
+  authDomain: "sistema-clinica-5c5b5.firebaseapp.com",
+  projectId: "sistema-clinica-5c5b5",
+  storageBucket: "sistema-clinica-5c5b5.firebasestorage.app",
+  messagingSenderId: "830320583010",
+  appId: "1:830320583010:web:630f5009bd3aad85a54c3b",
+  measurementId: "G-FZMNED3F84"
 };
 
 const app = initializeApp(firebaseConfig);
